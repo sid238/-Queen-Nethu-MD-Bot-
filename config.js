30e84c9e00d521f159fff3d0cd68ec3863986342
sid238 <131943405+sid238@users.noreply.github.com>
2023-06-14 10:17:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,28 +3,28 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/co
 
 
 //═══════[Required Variables]════════\\
-global.owner = process.env.OWNER_NUMBER.split(",")
-global.mongodb = process.env.MONGODB_URI || "Enter-MongoURI-HERE"
+global.owner = process.env.OWNER_NUMBER.split("919142294671")
+global.mongodb = process.env.MONGODB_URI || "mongodb+srv://Sid:bhaihu@cluster0.il3yus6.mongodb.net/?retryWrites=true&w=majority"
 global.port= process.env.PORT || 5000
-global.email = 'shashikachirath1@gmail.com'
-global.github = 'https://github.com/mrhansamala/-Queen-Nethu-MD-Bot-'
-global.location = 'Sultanpur IN'
+global.email = 'cashearn120@gmail.com'
+global.github = 'https://github.com/sid238/-Queen-Nethu-MD-Bot-'
+global.location = 'bihar IN'
 global.gurl = 'https://instagram.com/' // add your username
-global.sudo = process.env.SUDO || '94781708673'
-global.devs = '94781708673';
-global.website = 'https://github.com/mrhansamala/-Queen-Nethu-MD-Bot-' //wa.me/+91000000000000
+global.sudo = process.env.SUDO || '919142294671'
+global.devs = '919142294671';
+global.website = 'https://github.com/sid238/-Queen-Nethu-MD-Bot-' //wa.me/+919142294671
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/d229929fd0e975d64010c.jpg'
 module.exports = {
-  botname: process.env.BOT_NAME || 'Queen Nethu',
-  ownername:process.env.OWNER_NAME || 'MR.Hansamala',
+  botname: process.env.BOT_NAME || 'GARENA FF BOT',
+  ownername:process.env.OWNER_NAME || 'BZR SID',
   sessionName: process.env.SESSION_ID || 'PUT-HERE',
   author: process.env.PACK_INFO.split(";")[0] || 'author', 
   auto_read_status : process.env.AUTO_READ_STATUS || 'false',
   packname: process.env.PACK_INFO.split(";")[1] || 'Name',
-  autoreaction: process.env.AUTO_REACTION || 'off',
+  autoreaction: process.env.AUTO_REACTION || 'ON',
   antibadword : process.env.ANTI_BAD_WORD || 'nobadwordokey',
   alwaysonline: process.env.ALWAYS_ONLINE || 'false',
-  antifake : process.env.FAKE_COUNTRY_CODE || '',
+  antifake : process.env.FAKE_COUNTRY_CODE || '212',
   readmessage: process.env.READ_MESSAGE || false,
   HANDLERS: process.env.PREFIX || ['.'],
   warncount : process.env.WARN_COUNT || 3,
@@ -33,7 +33,7 @@ module.exports = {
   antilink: process.env.ANTILINK_VALUES || 'chat.whatsapp.com',
   antilinkaction: process.env.ANTILINK_ACTION || 'remove',
   BRANCH: 'main',
-  ALIVE_MESSAGE: process.env.ALIVE_MESSAGE || '',
+  ALIVE_MESSAGE: process.env.ALIVE_MESSAGE || 'I AM ALIVE NOW',
   OPENAI_API_KEY: process.env.OPENAI_API_KEY || 'OPENAI_KEY',
   VERSION: process.env.VERSION === undefined ? 'v.0.0.3' : process.env.VERSION,
   LANG: process.env.THEME|| 'SECKTOR',
